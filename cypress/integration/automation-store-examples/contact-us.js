2f5a3465c3b0f0a52e760acc7bd76867ba11bc65
Alex Cândido <alex.candido@liferay.com>
2021-03-08 00:43:22 -0300

    Add happy path test for form submtion via Automation Test Store using xpath selectors

--- cypress/integration/automation-store-examples/contact-us.js
+++ cypress/integration/automation-store-examples/contact-us.js
@@ -6,5 +6,15 @@ describe("Test Contact Us form via Automation Test Store", () => {
 
         cy.visit("https://automationteststore.com/");
 
+        cy.xpath("//a[contains(text(),'Contact Us')]").click();
+
+        cy.xpath("//input[@id='ContactUsFrm_first_name']").type("First");
+
+        cy.xpath("//input[@id='ContactUsFrm_email']").type("email@domain.com");
+
+        cy.xpath("//textarea[@id='ContactUsFrm_enquiry']").type("enquiry");
+
+        cy.xpath("//button[contains(@title, 'Submit')]").click();
+
     });
 });
