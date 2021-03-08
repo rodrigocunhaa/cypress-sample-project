63f9b7373889df6352cbd57ff4e44598e5d8a098
Alex Cândido <alex.candido@liferay.com>
2021-03-07 21:33:13 -0300

    Add test of automation-store-examples

---
+++ cypress/integration/automation-store-examples/contact-us.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+
+    it("Should be able to submit the form", () => {
+
+        cy.visit("https://automationteststore.com/");
+
+    });
+});
