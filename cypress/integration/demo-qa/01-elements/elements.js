f657c71f3add547a95fb8c0b8d3ef0e8268b47a8
Alex Cândido <alex.candido@liferay.com>
2021-05-10 21:34:34 -0300

    Add Should be able to fill Checkbox

--- cypress/integration/demo-qa/01-elements/elements.js
+++ cypress/integration/demo-qa/01-elements/elements.js
@@ -19,10 +19,22 @@ describe("Test Elements on Demo QA site", () => {
         cy.xpath("//p[@id='name']").should('include.text', 'Diamond Farley');
 
         cy.xpath("//p[@id='email']").should('include.text', 'dfarley@demoqa.com');
-        
+
         cy.xpath("//p[@id='currentAddress']").should('include.text', '986 Golden Star Ave.Oxford, MS 38655');
 
         cy.xpath("//p[@id='permanentAddress']").should('include.text', '90 E. St Margarets St.Springfield, PA 19064');
+
+    });
+
+    it("Should be able to fill Checkbox", () => {
+
+        cy.visit("https://www.demoqa.com/checkbox");
+
+        cy.xpath("//button[contains(@class,'option-expand-all')]").click();
+
+        cy.xpath("//label[@for='tree-node-private']").click();
+
+        cy.xpath("//div[@id='result']").should('include.text', 'private');
 
     });
 });
