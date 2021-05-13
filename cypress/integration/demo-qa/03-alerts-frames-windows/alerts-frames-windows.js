02ac180f6cd0b144cf907368a3c487bf1a804648
Alex Cândido <alex.candido@liferay.com>
2021-05-13 01:10:42 -0300

    Add Should be able to interact with modals - Small and Large modals

--- cypress/integration/demo-qa/03-alerts-frames-windows/alerts-frames-windows.js
+++ cypress/integration/demo-qa/03-alerts-frames-windows/alerts-frames-windows.js
@@ -70,5 +70,23 @@ describe("Test Alerts, Frames and Windows on Demo QA site", () => {
 
     });
 
+    it("Should be able to interact with modals - Small and Large modals", () => {
+
+        cy.visit("https://www.demoqa.com/modal-dialogs");
+
+        cy.xpath("//button[@id='showSmallModal']").click();
+
+        cy.xpath("//div[@class='modal-body']").should('include.text', 'This is a small modal. It has very less content');
+
+        cy.xpath("//button[@id='closeSmallModal']").click();
+
+        cy.xpath("//button[@id='showLargeModal']").click();
+
+        cy.xpath("//div[@class='modal-body']").should('include.text', "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+
+        cy.xpath("//button[@id='closeLargeModal']").click();
+
+    });
+
 });
 
