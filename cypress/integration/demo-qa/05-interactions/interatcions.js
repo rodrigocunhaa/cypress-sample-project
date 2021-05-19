54ec29b37c8098d4505c96c34d43f9635cdccfa3
Alex Cândido <alex.candido@liferay.com>
2021-05-19 02:23:52 -0300

    WIP drag interactions on seleniumeasy site working properly

--- cypress/integration/demo-qa/05-interactions/interatcions.js
+++ cypress/integration/demo-qa/05-interactions/interatcions.js
@@ -10,4 +10,12 @@ describe("Test Interactions on Demo QA site", () => {
 
     });
 
+    it("Should be able to do Droppable interactions --- seleniumeasy site", () => {
+
+        cy.visit("https://www.seleniumeasy.com/test/drag-and-drop-demo.html");
+
+        cy.get('#todrag>span:nth-child(2)').drag('#mydropzone', 'center');
+
+    });
+
 });
